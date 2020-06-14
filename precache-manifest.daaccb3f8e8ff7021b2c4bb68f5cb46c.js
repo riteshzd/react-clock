a95a4fc97b637b254cc0623205542193be20ee15
Ritesh <riteshdash305@gmail.com>
2020-06-14 14:27:44 +0530

    Updates

---
+++ precache-manifest.daaccb3f8e8ff7021b2c4bb68f5cb46c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22da8d58483e6daa1a6dc02876436964",
+    "url": "/react-clock/index.html"
+  },
+  {
+    "revision": "218eb4d1a8809aff4575",
+    "url": "/react-clock/static/css/main.e19b5ac4.chunk.css"
+  },
+  {
+    "revision": "43dd0024fb929973d016",
+    "url": "/react-clock/static/js/2.7790c090.chunk.js"
+  },
+  {
+    "revision": "f454f3f7c50bca632558f77e0f1343f1",
+    "url": "/react-clock/static/js/2.7790c090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218eb4d1a8809aff4575",
+    "url": "/react-clock/static/js/main.24a86e92.chunk.js"
+  },
+  {
+    "revision": "313843e9ae8a04c6d384",
+    "url": "/react-clock/static/js/runtime-main.f492c042.js"
+  }
+]);
